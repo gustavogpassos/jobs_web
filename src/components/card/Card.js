2b5f78eb2cba455730eb234147562aec4d167b83
Gustavo G Passos <girardi_gustavo@outlook.com>
2023-02-19 22:02:46 -0300

    testes iniciais

---
+++ src/components/card/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import "./Card.css"
+class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                <h1>Card</h1>
+            </div>
+        )
+    }
+}
+
+export default Card
